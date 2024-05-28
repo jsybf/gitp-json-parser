dc933355d03b9a68f89f15594a5ba77b283d5325
park jun hyoung <gitpdeathwa@yonsei.ac.kr>
2024-05-28 17:45:32 +0900

    1. fixed bug in StringReader 2. add array parser 3. add array parser test case

--- src/main/kotlin/ValueParser.kt
+++ src/main/kotlin/ValueParser.kt
@@ -1,18 +1,20 @@
 package gitp
 
+import gitp.parser.ArrayParser
 import gitp.parser.ObjectParser
 import gitp.parser.StringParser
 
-object ValueReader {
+object ValueParser {
     /**
      * determined parser base on form of text and return parsed value
      * kind of dispatcher
      * @param reader: current of reader should point out starting point of json value
      */
-    fun read(reader: StringReader): Any? {
+    fun parse(reader: StringReader): Any? {
         return when (reader.current()) {
             '{' -> ObjectParser.parse(reader)
             '"' -> StringParser.parse(reader)
+            '[' -> ArrayParser.parse(reader)
             else -> throw IllegalStateException("unexpected char:${reader.current()}")
         }
     }
